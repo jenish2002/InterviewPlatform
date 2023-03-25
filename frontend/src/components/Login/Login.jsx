a65a5bc51e7ec25a7679d36f254dc3406cf48274
JenishPatel222 <jenish.patel@rapidops.com>
2023-03-25 17:26:43 +0530

    login ui

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "./login-background.jpg";
+import googleIcon from "./google-icon.png"
+
+function Login() {
+  const onSignIn = () => {};
+  return (
+    <div
+      className="d-flex vh-100 vw-100 align-items-center text-light ps-5"
+      style={{ backgroundImage: `url(${Image})` }}
+    >
+      <div className="d-flex align-items-start text-container flex-column flex-fill">
+        <h1 className="mb-2">Welcome to</h1>
+        <h1 className="mb-5">Interview Platform</h1>
+        <p className="mb-5 fs-2">Start your Journey Today ..!!</p>
+        <button
+          onClick={onSignIn}
+          className="btn btn-dark sign-in-button fs-3 ps-4 pt-2 pb-2 pe-4 text-light mt-5 mb-5 d-flex align-items-center justify-content-between"
+        >
+          <img src={googleIcon} className="me-3 google-icon" height={35} width={35} />
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
