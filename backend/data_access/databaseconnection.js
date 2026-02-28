e7737cb2d33722c6ab1da9c762839fac2981a2b9
Jenish Patel <60685502+jenish2002@users.noreply.github.com>
2026-02-28 14:39:15 +0530

    Remove url

--- backend/data_access/databaseconnection.js
+++ backend/data_access/databaseconnection.js
@@ -1,6 +1,5 @@
 const mongoose= require('mongoose');
-// const mongoURI="mongodb+srv://vatsal2401:Vatsal9393@cluster0.800wcdy.mongodb.net/CloudCar?retryWrites=true&w=majority"
-const mongoURI="mongodb+srv://vatsal2401:Vatsal9393@cluster0.800wcdy.mongodb.net/InterviewPlatform?retryWrites=true&w=majority"
+const mongoURI=""
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,{  useNewUrlParser: true,
